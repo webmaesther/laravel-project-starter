989b36ae982dd028fd48c22d01fe2f5a12465672
Eszter Czotter <eszter.czotter@gmail.com>
2025-06-03 10:56:43 +0200

    Configure Rector

--- config/solo.php
+++ config/solo.php
@@ -10,7 +10,7 @@ use SoloTerm\Solo\Themes;
 
 // Solo may not (should not!) exist in prod, so we have to
 // check here first to see if it's installed.
-if (! class_exists('\SoloTerm\Solo\Manager')) {
+if (! class_exists(SoloTerm\Solo\Manager::class)) {
     return [
         //
     ];
