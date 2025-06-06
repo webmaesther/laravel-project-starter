73de7a579e4fb0075a8b8aca647feb91563aa0f6
Eszter Czotter <eszter.czotter@gmail.com>
2025-06-06 06:17:08 +0200

    Install Laravel Socialite

--- config/services.php
+++ config/services.php
@@ -37,4 +37,12 @@ return [
         ],
     ],
 
+    // Socialite Services
+
+    // 'github' => [
+    //     'client_id' => env('GITHUB_CLIENT_ID'),
+    //     'client_secret' => env('GITHUB_CLIENT_SECRET'),
+    //     'redirect' => 'http://example.com/callback-url',
+    // ],
+
 ];
