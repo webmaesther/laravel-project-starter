1c887a465557eb4ffadc4abd8256cc8075335d56
Eszter Czotter <eszter.czotter@gmail.com>
2025-06-06 20:34:38 +0200

    Remove missing inertia props from the frontend type declaration

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -13,7 +13,5 @@ export interface User {
 
 export type AppPageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
     name: string;
-    quote: { message: string; author: string };
     auth: Auth;
-    sidebarOpen: boolean;
 };
