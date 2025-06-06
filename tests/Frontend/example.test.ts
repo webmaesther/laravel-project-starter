c3b7781a96a6e0154d3e8aaf0a6816ade7d03808
Eszter Czotter <eszter.czotter@gmail.com>
2025-06-06 06:27:19 +0200

    Install Vitest

---
+++ tests/Frontend/example.test.ts
@@ -0,0 +1,8 @@
+import {describe} from 'node:test';
+import {expect, test} from 'vitest';
+
+describe('Example', () => {
+    test('does something', async () => {
+        expect(true).toBe(true);
+    });
+})
