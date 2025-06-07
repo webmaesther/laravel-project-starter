ea28504324b24de7206023f6531223d2416ed555
Eszter Czotter <eszter.czotter@gmail.com>
2025-06-07 15:57:20 +0200

    Configure the API to be stateful

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,9 +16,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: [
-            HandleInertiaRequests::class,
-        ]);
+        $middleware
+            ->statefulApi()
+            ->web(append: [
+                HandleInertiaRequests::class,
+            ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
