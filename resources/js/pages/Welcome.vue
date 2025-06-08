047d91a3395b601b351e757a39dade80c631416e
Eszter Czotter <eszter.czotter@gmail.com>
2025-06-08 17:52:40 +0200

    Tweak the UI on the welcome page

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-    //
+    import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
     <div class="bg-base-300 flex h-screen w-screen items-center justify-center">
+        <Head title="Welcome"></Head>
         <div class="card card-border bg-base-100 m-6 w-96 drop-shadow-xl">
             <div class="card-body flex items-center">
                 <h1 class="card-title text-3xl">Welcome!</h1>
                 <label class="input mt-6 w-full">
-                    <span class="sr-only">Email</span>
-                    <input type="text" class="grow" placeholder="Email" />
+                    <span>Email</span>
+                    <input type="text" class="grow" placeholder="your.email@example.com" />
                 </label>
                 <div class="card-actions"></div>
                 <div class="grid w-full grid-cols-2 justify-around gap-2">
@@ -17,6 +18,7 @@
                     <button class="btn">Register</button>
                 </div>
                 <div class="divider">OR</div>
+                <p class="text-sm">Confirm your identity with:</p>
                 <a class="btn btn-block">
                     <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                         <g>
@@ -27,13 +29,13 @@
                             <path fill="currentColor" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path>
                         </g>
                     </svg>
-                    Login with Google
+                    Google
                 </a>
                 <a class="btn btn-block">
                     <svg aria-label="Facebook logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
                         <path fill="currentColor" d="M8 12h5V8c0-6 4-7 11-6v5c-4 0-5 0-5 3v2h5l-1 6h-4v12h-6V18H8z"></path>
                     </svg>
-                    Login with Facebook
+                    Facebook
                 </a>
                 <!--<a class="btn btn-block">-->
                 <!--    <svg aria-label="X logo" width="16" height="12" viewBox="0 0 300 271" xmlns="http://www.w3.org/2000/svg">-->
@@ -42,7 +44,7 @@
                 <!--            d="m236 0h46l-101 115 118 156h-92.6l-72.5-94.8-83 94.8h-46l107-123-113-148h94.9l65.5 86.6zm-16.1 244h25.5l-165-218h-27.4z"-->
                 <!--        />-->
                 <!--    </svg>-->
-                <!--    Login with X-->
+                <!--    X-->
                 <!--</a>-->
                 <!--<a class="btn btn-block">-->
                 <!--    <svg aria-label="Apple logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1195 1195">-->
@@ -51,7 +53,7 @@
                 <!--            d="M1006.933 812.8c-32 153.6-115.2 211.2-147.2 249.6-32 25.6-121.6 25.6-153.6 6.4-38.4-25.6-134.4-25.6-166.4 0-44.8 32-115.2 19.2-128 12.8-256-179.2-352-716.8 12.8-774.4 64-12.8 134.4 32 134.4 32 51.2 25.6 70.4 12.8 115.2-6.4 96-44.8 243.2-44.8 313.6 76.8-147.2 96-153.6 294.4 19.2 403.2zM802.133 64c12.8 70.4-64 224-204.8 230.4-12.8-38.4 32-217.6 204.8-230.4z"-->
                 <!--        ></path>-->
                 <!--    </svg>-->
-                <!--    Login with Apple-->
+                <!--    Apple-->
                 <!--</a>-->
                 <!--<a class="btn btn-block">-->
                 <!--    <svg aria-label="Microsoft logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">-->
@@ -60,7 +62,7 @@
                 <!--        <path d="M96 265H247V416H96" fill="currentColor"></path>-->
                 <!--        <path d="M265 265H416V416H265" fill="currentColor"></path>-->
                 <!--    </svg>-->
-                <!--    Login with Microsoft-->
+                <!--    Microsoft-->
                 <!--</a>-->
                 <!--<a class="btn btn-block">-->
                 <!--    <svg aria-label="LinkedIn logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">-->
@@ -70,7 +72,7 @@
                 <!--            fill-rule="evenodd"-->
                 <!--        ></path>-->
                 <!--    </svg>-->
-                <!--    Login with LinkedIn-->
+                <!--    LinkedIn-->
                 <!--</a>-->
                 <!--<a class="btn btn-block">-->
                 <!--    <svg aria-label="GitHub logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">-->
@@ -79,7 +81,7 @@
                 <!--            d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z"-->
                 <!--        ></path>-->
                 <!--    </svg>-->
-                <!--    Login with GitHub-->
+                <!--    GitHub-->
                 <!--</a>-->
                 <!--<a class="btn btn-block">-->
                 <!--    <svg aria-label="Slack logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">-->
@@ -90,7 +92,7 @@
                 <!--            <path stroke="currentColor" d="M110 305h.1v.1m97 0v97"></path>-->
                 <!--        </g>-->
                 <!--    </svg>-->
-                <!--    Login with Slack-->
+                <!--    Slack-->
                 <!--</a>-->
                 <!--<a class="btn btn-block">-->
                 <!--    <svg aria-label="Amazon logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">-->
@@ -103,7 +105,7 @@
                 <!--            ></path>-->
                 <!--        </g>-->
                 <!--    </svg>-->
-                <!--    Login with Amazon-->
+                <!--    Amazon-->
                 <!--</a>-->
             </div>
         </div>
