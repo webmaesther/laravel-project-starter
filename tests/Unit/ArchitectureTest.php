2c65981dcdf3b272c2713593bedc60f7953ab26b
Eszter Czotter <eszter.czotter@gmail.com>
2025-06-12 18:56:39 +0200

    Ensure controllers do not have wonky method names

--- tests/Unit/ArchitectureTest.php
+++ tests/Unit/ArchitectureTest.php
@@ -6,6 +6,19 @@ arch()->preset()->php();
 arch()->preset()->security();
 
 arch('modular laravel', function (): void {
-    expect('App\Http\Controllers')->toHaveSuffix('Controller')
-        ->and('App\*\Http\Controllers')->toHaveSuffix('Controller');
+    expect('App\Http\Controllers')
+        ->not->toBeClasses()
+        ->and('App\*\Http\Controllers')
+        ->toHaveSuffix('Controller')
+        ->toExtendNothing()
+        ->not->toHavePublicMethodsBesides([
+            '__invoke',
+            'index',
+            'show',
+            'create',
+            'store',
+            'edit',
+            'update',
+            'destroy',
+        ]);
 });
