4ff4054813b16a20aef1ce1e2001ad5a16775858
Eszter Czotter <eszter.czotter@gmail.com>
2025-06-29 14:00:36 +0200

    refactor: use a controller for login links

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,9 @@ declare(strict_types=1);
 use App\Enums\SocialiteDriver;
 use App\Http\Controllers\Identity\CallbackController;
 use App\Http\Controllers\Identity\RedirectController;
+use App\Http\Controllers\LoginLinkController;
 use App\Http\Middleware\InitializePaddle;
 use App\Http\Middleware\RedirectLocalHost;
-use App\Notifications\MagicLink;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => inertia('Home', [
@@ -26,20 +25,10 @@ Route::prefix('{driver}')
 
 Route::get('/dashboard', fn () => inertia('Dashboard'))->middleware('auth')->name('dashboard');
 
-Route::post('/login/link', function (Request $request) {
-    $user = App\Models\User::query()
-        ->where('email', $request->string('email'))
-        ->firstOrFail();
-
-    $user->notify(new MagicLink($user));
-
-    return redirect()->back()->with([
-        'toast' => 'Magic link sent. Please check your email.',
-    ]);
-})->name('login.link.store');
-
-Route::get('/login/link/{user}', function (App\Models\User $user) {
-    Illuminate\Support\Facades\Auth::login($user);
-
-    return to_route('dashboard');
-})->middleware('signed')->name('login.link.show');
+Route::prefix('/login/link')
+    ->name('login.link.')
+    ->controller(LoginLinkController::class)
+    ->group(function () {
+        Route::post('/', 'store')->name('store');
+        Route::get('{user}', 'show')->name('show')->middleware('signed');
+    });
