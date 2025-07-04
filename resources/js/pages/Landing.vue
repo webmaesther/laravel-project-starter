f2e7f00c9e0836941365b7f91dbd6182727f7847
Eszter Czotter <eszter.czotter@gmail.com>
2025-07-04 17:59:00 +0200

    revert: remove the preconfigured subscriptions

--- resources/js/pages/Landing.vue
+++ resources/js/pages/Landing.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-    import SubscriptionPlan from '@/components/SubscriptionPlan.vue';
-
     defineProps<{
         plans: {
             id: string;
@@ -24,11 +22,6 @@
                 <p class="mb-5">The all-in-one Project Starter Kit.</p>
                 <button class="btn btn-primary">Ready, set, go!</button>
             </div>
-        </div>
-    </div>
-    <div class="hero bg-base-200 min-h-screen">
-        <div class="hero-content text-center">
-            <SubscriptionPlan v-for="plan in plans" :key="plan.id" :plan />
         </div>
     </div>
 </template>
