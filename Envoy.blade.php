6ad70b0a70afd525bcb807d8304c40e5bb2054f3
Eszter Czotter <eszter.czotter@gmail.com>
2025-07-24 07:25:12 +0200

    chore: use an envoy task to check and update dependencies instead of ci

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,1 +1,11 @@
-<!-- Define Envoy commands -->
+@servers(['localhost' => '127.0.0.1'])
+
+@task('outdated', ['on' => 'localhost'])
+npm run deps:check
+composer deps:check
+@endtask
+
+@task('update', ['on' => 'localhost'])
+npm run deps:bump
+composer deps:bump
+@endtask
